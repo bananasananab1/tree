de632d8ce6224a2e8232e39568edccb5c5677958
bananasananab1 <aarongallowaywaldon@gmail.com>
2024-04-16 16:00:11 -0600

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Banana Tree",
+	id: "bananas",
+	author: "me",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
